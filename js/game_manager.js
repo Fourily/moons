9d33b5abeac73b54c9b1c1922c5f252a18bf7974
Fourily <114097406+Fourily@users.noreply.github.com>
2022-12-09 15:56:51 +0000

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.999999 ? Math.random() < 0.999 ? Math.random() < 0.98 ? Math.random() < 0.96 ? ? Math.random() < 0.9 ? 1 : 2 : 3 : 4 : 5 : 6;
+    var value = Math.random() < 0.999999 ? Math.random() < 0.999 ? Math.random() < 0.98 ? Math.random() < 0.96 ? Math.random() < 0.9 ? 1 : 2 : 3 : 4 : 5 : 6;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
